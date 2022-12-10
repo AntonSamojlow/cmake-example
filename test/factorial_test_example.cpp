5ab7a267139d10e73b501a1c4e871a93b0056ae2
AntonSamojlow <anton.samojlow@gmail.com>
2022-12-10 18:46:41 +0100

    add a failing test

--- test/factorial_test_example.cpp
+++ test/factorial_test_example.cpp
@@ -7,9 +7,14 @@ namespace
     }
 
 }
+
 TEST_CASE("Factorial Example Test", "[factorial]") {
     REQUIRE(Factorial(1) == 1);
     REQUIRE(Factorial(2) == 2);
     REQUIRE(Factorial(3) == 6);
     REQUIRE(Factorial(10) == 3628800);
+}
+
+TEST_CASE("Factorial Example Test Failing", "[factorial-fail]") {
+    REQUIRE(Factorial(1) == 0);
 }
