350d1d5d75d1e5a330c44d4ff29db1f9739641e0
AntonSamojlow <anton.samojlow@gmail.com>
2022-12-27 16:54:18 +0100

    updated formatting

--- src/main/mainclass.h
+++ src/main/mainclass.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <string>
 
-struct MainClass
-{
-	int value = 0;
-	bool is_odd = false;
-	void recompute_flag();
-	std::string stringify();
-private:
-	bool value_is_odd();
+struct MainClass {
+  int value = 0;
+  bool is_odd = false;
+  void recompute_flag();
+  std::string stringify();
+
+ private:
+  bool value_is_odd();
 };
