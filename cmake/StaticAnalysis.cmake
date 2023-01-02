d1602acd605cc182fb9adff0821cfdac880d3078
Anton Samojlow <Anton.Samojlow@web.de>
2023-01-02 23:53:11 +0100

    added comments

--- cmake/StaticAnalysis.cmake
+++ cmake/StaticAnalysis.cmake
@@ -15,11 +15,12 @@ macro(enable_clang_tidy)
 
   # construct the clang-tidy command line
   # adding -extra-arg=-Wno-unused-command-line-argument due to https://github.com/llvm/llvm-project/issues/53674
-  # (which appeared on github with clang-tidy 14.0.6 but not locally with 15.0.1)
+  # (which appeared with on github runners with msvc and clang-tidy 14.0.6 but not locally with 15.0.1)
+  # (TODO investigate: with lower version locally and report back)
   set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY} 
     "-extra-arg=-Wno-unknown-warning-option;-extra-arg=-Wno-unused-command-line-argument")
   
-    # set all warnings as errors, if enabled
+  # set all warnings as errors, if enabled
   if(${WARNINGS_AS_ERRORS})
     message(STATUS "enable_clang_tidy_globally: WARNINGS_AS_ERRORS enabled")
     list(APPEND CMAKE_CXX_CLANG_TIDY -warnings-as-errors=*)
